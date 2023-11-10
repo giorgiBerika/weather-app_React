7dfae33ffdaa0f8fb974db5ae24fd00d5fdf2ecc
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-11-10 21:56:32 +0400

    Adding components' directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState, useCallback } from 'react'
+import './App.scss'
+// https://api.openweathermap.org/data/2.5/weather?units=metric&q=London&appid=b1af9dc988407783a9a7c41508074fdf
 function App() {
   const [count, setCount] = useState(0)
-
+   
+  
   return (
     <>
-      
+      <div className="my-app">
+        Hello there
+      </div>
     </>
   )
 }
