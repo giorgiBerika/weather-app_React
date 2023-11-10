0399fe87705e58f94ed39c40da5546f926ff7951
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-11-10 22:29:14 +0400

    Adding data fetching function, by axios library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState, useCallback } from 'react'
+import { useState, useEffect,useRef } from 'react'
+import axios from 'axios'
 import './App.scss'
 // https://api.openweathermap.org/data/2.5/weather?units=metric&q=London&appid=b1af9dc988407783a9a7c41508074fdf
 function App() {
   const [count, setCount] = useState(0)
-   
+  const [currentLocation, setCurrentLocation] = useState('London')
+  const apiKey = useRef('1c484b86bfbb859e8eeb46bdefcaf5dd')
+  useEffect(() => {
+
+
+    async function getWeatherInfo() 
+     {
+      try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${currentLocation}&appid=${apiKey.current}`)
+            console.log(response)
+      }
+      catch(error){
+        console.log(`Error fetching data:`,error)
+      }
+    };
+
+    // getWeatherInfo();
+  } ,[] );
   
+
   return (
     <>
       <div className="my-app">
